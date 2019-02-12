067b295ff05e4b031d8bf8dd2050e3e35d1f3218
longphamref1 <39331651+longphamref1@users.noreply.github.com>
2019-02-12 16:25:51 +0700

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp2/Excel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace WindowsFormsApp2
+{
+    class Excel
+    {
+        string path = "";
+        _Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+
+        public Excel()
+        {
+
+        }
+
+        public Excel(string path, int Sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[Sheet];
+        }
+
+        public void CreateNewFile()
+        {
+            this.wb = excel.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+            this.ws = wb.Worksheets[1];
+        }
+
+        public void CreateNewSheet()
+        {
+            Worksheet tempsheet = wb.Worksheets.Add(After: ws);
+        }
+
+        public string ReadCell(int i, int j)
+        {
+            i++;
+            j++;
+            if (ws.Cells[i, j].Value2 != null)
+                return ws.Cells[i, j].Value2;
+            else
+                return "askfsf";
+        }
+
+        public string[,] ReadRange(int starti, int starty, int endi, int endy)
+        {
+            Range range = (Range)ws.Range[ws.Cells[starti, starty], ws.Cells[endi, endy]];
+            object[,] holder = range.Value2;
+            string[,] returnstring = new string[endi - starti, endy - starty];
+            for (int p = 1; p <= endi - starti; p++)
+            {
+                for (int q = 1; q <= endy - starty; q++)
+                {
+                    returnstring[p - 1, q - 1] = holder[p, q].ToString();
+                }
+            }
+            return returnstring;
+        }
+
+        public void WriteToCell(int i, int j, string s)
+        {
+            i++;
+            j++;
+            ws.Cells[i, j].Value2 = s;
+        }
+
+        public void WriteRange (int starti, int starty, int endi, int endy, string[,] writestring)
+        {
+            Range range = (Range)ws.Range[ws.Cells[starti, starty], ws.Cells[endi, endy]];
+            range.Value2 = writestring;
+        }
+
+        public void SelectWorksheet(int SheetNumber)
+        {
+            this.ws = wb.Worksheets[SheetNumber];
+        }
+
+        public void DeleteWorksheet(int SheetNumber)
+        {
+            wb.Worksheets[SheetNumber].Delete();
+        }
+
+        public void Save()
+        {
+            wb.Save();
+        }
+
+        public void SaveAs(string path)
+        {
+            wb.SaveAs(path);
+        }
+
+        public void Close()
+        {
+            wb.Close();
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CreateNewFile();
+        }
+
+        
+        public void OpenFile()
+        {
+            Excel excel = new Excel(@"C:\VAS_CDR_report\19.02.11\VAS.CDR.report.190211.xls", 1);
+            MessageBox.Show(excel.ReadCell(20, 0));
+        }
+
+        public void WriteData()
+        {
+            Excel excel = new Excel(@"C:\VAS_CDR_report\19.02.11\VAS.CDR.report.190211.xls", 1);
+            excel.WriteToCell(0, 10, "Test Write");
+            excel.Save();
+            excel.Close();
+            excel.SaveAs(@"C:\VAS_CDR_report\19.02.11\VAS.CDR.report.1902112.xls");
+            excel.Close();
+        }
+
+        public void CreateNewFile()
+        {
+            Excel ex = new Excel();
+            ex.CreateNewFile();
+            ex.SaveAs(@"C:\VAS_CDR_report\19.02.11\Test.xlsx");
+            ex.Close();
+        }
+    }
+}
